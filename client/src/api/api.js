4cc7a5543492844438a35fbaf8f5befdec2402d2
Jennifer Kiveisha <jennifer.is.my.name@gmail.com>
2024-03-18 14:19:48 +0200

    remove console.log

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,9 +4,6 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "http://localhost:8888/api",
 })
 
-console.log(api.baseURL);
-console.log(process.env.REACT_APP_API_URL);
-
 export const createUser = payload => api.post(`/user`, payload)
 export const getUserByUsernameAndPassword = (username, password) => api.get(`/user/${username}/${password}`)
 
